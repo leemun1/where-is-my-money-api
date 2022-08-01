478da7d1a4a06ef9f6bf87fa2592dee25ce6993c
Mike Lee <mikemunkyu.lee@gmail.com>
2022-08-01 18:30:25 -0400

    Remove unused endpoint

--- main.py
+++ main.py
@@ -41,12 +41,6 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
